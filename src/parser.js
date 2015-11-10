96512f8176fc5bd1c972de165afc7bcfe187b275
Jesper Håkansson <jesper@jesperh.se>
2015-11-10 15:02:36 +0100

    green

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,22 @@
+yaml = require('js-yaml');
+
 const parser = {
   parse(stringToParse) {
     if (stringToParse === undefined || typeof stringToParse !== 'string' || stringToParse.length <= 0) {
       throw new Error('The string to be parsed needs to be a string!');
     }
-    return {typings: ''};
+
+    const parsed = yaml.safeLoad(stringToParse);
+
+    for (var key in parsed) {
+      if (parsed.hasOwnProperty(key)) {
+        if (!parsed[key]) {
+          parsed[key] = '';
+        }
+      }
+    }
+
+    return parsed;
   }
 }
 
