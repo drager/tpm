01e6afad3f9db9f0f21ad053f287741b630e9880
Jesper Håkansson <jesper@jesperh.se>
2015-11-10 15:34:25 +0100

    red

--- test/specs/fetcherSpec.js
+++ test/specs/fetcherSpec.js
@@ -7,5 +7,12 @@ describe('fetcher', () => {
     it('should throw if the passed parameter is undefiend', () => {
       expect(fetcher.fetch).to.throw('The string to be parsed needs to be a string!');
     });
+
+    it('should throw if the passed parameter is not a string', () => {
+      const parameter = 1;
+      expect(() => {
+          fetcher.fetch(parameter);
+        }).to.throw('The string to be parsed needs to be a string!');
+    });
   });
 });
