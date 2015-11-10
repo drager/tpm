628edb1fe152e3673214c217b9e2377e36c7b5b0
Jesper Håkansson <jesper@jesperh.se>
2015-11-10 15:39:03 +0100

    green

--- src/fetcher.js
+++ src/fetcher.js
@@ -2,7 +2,8 @@
 
 const fetcher = {
   fetch(url) {
-    if (url === undefined || typeof url !== 'string') {
+    if (url === undefined || typeof url !== 'string' ||
+        url.length <= 0) {
       throw new Error('The url to be fetched needs to be a string!');
     }
   }
