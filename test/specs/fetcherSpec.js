c52fcd8efe274d1ad73279bf8b9e8059b5211ced
Jesper Håkansson <jesper@jesperh.se>
2015-11-10 15:32:31 +0100

    refactor

--- test/specs/fetcherSpec.js
+++ test/specs/fetcherSpec.js
@@ -5,7 +5,7 @@ const fetcher = require('../../src/fetcher');
 describe('fetcher', () => {
   describe('fetch', () => {
     it('should throw if the passed parameter is undefiend', () => {
-      expect(fetcher.fetch).to.throw(Error);
+      expect(fetcher.fetch).to.throw('The string to be parsed needs to be a string!');
     });
   });
 });
