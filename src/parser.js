167898d62194570ba9ca8d27476f8ad914b22f7f
Jesper Håkansson <jesper@jesperh.se>
2015-11-10 14:20:04 +0100

    green

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,6 @@
 const parser = {
   parse(stringToParse) {
-    if (stringToParse === undefined || typeof stringToParse !== 'string') {
+    if (stringToParse === undefined || typeof stringToParse !== 'string' || stringToParse.length <= 0) {
       throw new Error('The string to be parsed needs to be a string!');
     }
     return {typings: ''};
