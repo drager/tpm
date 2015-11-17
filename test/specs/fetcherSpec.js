af8dabfcb06d5efde6f382255c94cec13b2ab3d7
Jesper Håkansson <jesper@jesperh.se>
2015-11-17 13:58:52 +0100

    red

--- test/specs/fetcherSpec.js
+++ test/specs/fetcherSpec.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-
+const sinon = require('sinon');
 const fetcher = require('../../src/fetcher');
 
 describe('fetcher', () => {
@@ -20,6 +20,16 @@ describe('fetcher', () => {
       expect(() => {
           fetcher.get(urlToFetch);
         }).to.throw('The url to be fetched needs to be a string!');
+    });
+
+    it('should return a Promise', () => {
+      const urlToFetch = 'https://github.com/drager/tpm/';
+      const fetcherSpy = sinon.spy();
+      const promise = fetcher.get(urlToFetch);
+
+      return promise.then(fetcherSpy).then(() => {
+        expect(fetcherSpy).to.have.been.calledOnce;
+      });
     });
   });
 });
