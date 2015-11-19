80ad68768d4ee90bdba10d2d364767322381f6e6
Jesper Håkansson <jesper@jesperh.se>
2015-11-19 14:53:04 +0100

    green

--- src/typings.js
+++ src/typings.js
@@ -2,7 +2,8 @@
 
 const typings = {
   find(path) {
-    if (path === undefined || typeof path !== 'string') {
+    if (path === undefined || typeof path !== 'string' ||
+        path.length <= 0) {
       throw new Error('The path needs to be a string!');
     }
   },
