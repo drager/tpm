d934ab95bb1e6eab9463933f2bc173b7a91a5726
Jesper Håkansson <jesper@jesperh.se>
2015-11-19 16:07:17 +0100

    green

--- src/typings.js
+++ src/typings.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const fs = require('fs');
+
 const typings = {
   find(path) {
     if (path === undefined || typeof path !== 'string' ||
         path.length <= 0) {
       throw new Error('The path needs to be a string!');
     }
+
+    fs.lstatSync(path);
   },
 }
 
