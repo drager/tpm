4723df394d4e8f5097e6752b341d0f02dc923bf8
Jesper Håkansson <jesper@jesperh.se>
2015-11-27 13:43:19 +0100

    red/green: Making 'should return a rejected Promise if reading failes' green causes 'should return a resolved Promise' fail because it doesnt send back data, will be fixed in refactor

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ const tpm = () => {
   if (typings.folderExists(yamlFile)) {
     return new Promise((resolve, reject) => {
       fs.readFile(yamlFile, 'utf8', (error, data) => {
+        if (error) {
+          reject(error);
+        }
         resolve();
       });
       reject();
