5136aeab2b2d2d4784f324a95c2bbfcb62dc1067
Jesper Håkansson <jesper@jesperh.se>
2016-01-03 20:35:07 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -11,34 +11,40 @@ const yamlFile = './typings.yaml';
 const savePath = './typings_custom';
 
 const tpm = () => {
-  if (typings.folderExists(yamlFile)) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(yamlFile, 'utf8', (error, data) => {
-        if (error) {
-          reject(error);
-        }
+  if (!typings.folderExists(yamlFile)) {
+    throw new Error(`${yamlFile} is not present!`);
+  }
+  return new Promise((resolve, reject) => {
+    fs.readFile(yamlFile, 'utf8', (error, data) => {
+      if (error) {
+        reject(error);
+      }
 
-        const parsed = parser.parse(data);
-        const object = parsed.typings;
-        const keys = Object.keys(object);
+      const parsed = parser.parse(data);
+      const object = parsed.typings;
 
-        const promises = keys.map((key) => {
-          let url = `https://github.com/${object[key]}`;
-          return fetcher.get(url, key).then((file) => {
-            console.info('Fetching:', url)
-            typings.find(file).then((files) => {
-              console.info('Found typings...');
-              typings.move(files, Path.normalize(`${savePath}/${key}`));
-              console.info('Moving typings...');
-              console.info('Done.');
-            });
+      if (!object) {
+        throw new Error(`Typings are missing in the ${yamlFile} file!`);
+      }
+
+      const keys = Object.keys(object);
+
+      const promises = keys.map((key) => {
+        let url = `https://github.com/${object[key]}`;
+        console.info('Fetching:', url)
+        return fetcher.get(url, key).then((file) => {
+          console.info('Found typings...');
+          typings.find(file).then((files) => {
+            typings.move(files, Path.normalize(`${savePath}/${key}`));
+            console.info('Moving typings...');
+            console.info('Done.');
           });
         });
-        Promise.all(promises).then(resolve);
       });
-      reject();
+      Promise.all(promises).then(resolve);
     });
-  }
+    reject();
+  });
 }
 
 module.exports = tpm;
